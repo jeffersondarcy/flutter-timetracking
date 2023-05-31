789c2c5c3959b8583f3ae7083b74912e176a6d36
Jewgenij Steinhart <4200365+jeffersondarcy@users.noreply.github.com>
2023-05-31 13:54:41 +0200

    add ProviderScope

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'widgets/timer_page.dart';
 
-
 void main() {
-  runApp(const App());
+  runApp(
+    const ProviderScope(
+      child: const App(),
+    ),
+  );
 }
 
 class App extends StatelessWidget {
